02719cb36555dd1a7075f6e757172013eb9ae2ab
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-07 16:21:30 -0400

    feat(global): crea rutas

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
-const routes: Routes = [];
+// localhost:4200/heroes/
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'new-hero', component: NewPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: 'list', component: ListPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: ':id', component: HeroPageComponent },
+      { path: '**', redirectTo: 'list' },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
