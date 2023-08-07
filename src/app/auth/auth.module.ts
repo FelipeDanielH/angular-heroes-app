1fe07760a33fd73ca707d24cbbe789c806832a61
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-07 12:25:22 -0400

    fix: elimina modulo innexistente

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
-import { LayoutComponent } from './pages/layout/layout.component';
+
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    LayoutComponent,
     LayoutPageComponent,
     LoginPageComponent,
     RegisterPageComponent
