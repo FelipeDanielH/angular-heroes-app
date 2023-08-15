eb9507f45af5c1d39bae85824b1ec86448890079
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-15 10:40:28 -0400

    feat: agrega forControlName a formulario

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -7,8 +9,26 @@ import { Component } from '@angular/core';
   ]
 })
 export class NewPageComponent {
+
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>(''),
+    publisher: new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  })
+
   public publishers = [
-    { id: 'DC Comics', desc: 'DC - Comics'},
-    { id: 'Marvel Comics', desc: 'Marvel - Comics'}
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' }
   ]
+
+  onSubmit():void {
+    console.log({
+      formIsValida: this.heroForm.valid,
+      value: this.heroForm.value,
+    })
+  }
 }
